e230ad1aaee2fe12057558db65841f9b487722ed
Jeanhwea <hujinghui@buaa.edu.cn>
2015-12-13 13:59:29 +0800

    Update qksort.c

--- sort/qksort.c
+++ sort/qksort.c
@@ -1,21 +1,22 @@
+
 void qksort(int *a, int left, int right)
 {
-    int i, j, t, p;
+    int i, j, pivot;
     if (left > right) return;
-    p=a[right]; i=left; j=right;
+    pivot=a[right]; i=left; j=right;
     while(i<j) {
         for(;i<j;i++)
-            if (a[i]>p) {
+            if (a[i]>pivot) {
                 a[j]=a[i];
                 break;
             }
         for(;j>i;j--)
-            if (a[j]<p) {
+            if (a[j]<pivot) {
                 a[i]=a[j];
                 break;
             }
     }
-    a[j]=p;
+    a[j]=pivot;
     qksort(a,left,j-1);
     qksort(a,j+1,right);
 }
