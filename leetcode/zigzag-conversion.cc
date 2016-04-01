03c19dcfda878a9b56cbf9d39a67eacd70a36328
Jeanhwea <hujinghui@buaa.edu.cn>
2016-04-01 16:07:43 +0800

    Update zigzag-conversion.cc

--- leetcode/zigzag-conversion.cc
+++ leetcode/zigzag-conversion.cc
@@ -2,27 +2,24 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         string ret;
-        int maxlen = s.length();
+        int strlen = s.length();
         if (numRows <= 1) return s;
         for (int i = 1; i <= numRows; i++) {
-            for (int j = 1; j <= maxlen; j++) {
-                int idx = idxconvert(i, j, numRows);
-                if (idx>0 && idx <= s.length())
+            for (int j = 1; j <= strlen; j++) {
+                int r = (j-1)%(numRows-1);
+                if (0!=r&&numRows-i!=r) continue; // 跳过空白
+
+                int idx = 0;
+                if (0==r) {
+                    // 计算主线上的元素
+                    idx = 2*(j-1)+i;
+                } else {
+                    // 计算对角线上的元素
+                    idx = 2*((j-r)-1)+numRows+r;
+                }
+                if (idx>0 && idx <= strlen)
                     ret.push_back(s[idx-1]);
             }
-        }
-        return ret;
-    }
-
-    static int idxconvert(int i, int j, int n) {
-        int q, r, ret;
-        q = (j-1)/(n-1), r = (j-1)%(n-1);
-        if (r == 0) {
-            ret = 2*(j-1)+i;
-        } else if (r==n-i){
-            ret = 2*q*(n-1)+n+n-i;
-        } else {
-            ret = 0;
         }
         return ret;
     }
