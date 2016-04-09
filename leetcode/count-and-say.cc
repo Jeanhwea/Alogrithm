84b79c004c25cf9d98ddb2682b381a56a75612d6
Jeanhwea <hujinghui@buaa.edu.cn>
2016-04-09 21:21:17 +0800

    Create count-and-say.cc

---
+++ leetcode/count-and-say.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ret("1"), next;
+        for (int i=1; i<n; ++i) {
+            int ch, count = 1;
+            ch = ret[0];
+            for (int j=0; j<ret.length(); ++j) {
+                if (j+1<ret.length() && ret[j+1]==ch) {
+                    ++count;
+                } else {
+                    next += (char)count+'0';
+                    next += ch;
+                    if (j+1<ret.length()) {
+                        ch=ret[j+1];
+                        count=1;
+                    }
+                }
+            }
+            ret = next;
+            next = "";
+        }
+        return ret;
+    }
+};
